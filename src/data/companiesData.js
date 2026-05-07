6c7fc9a342f87c380ca276243d6a3d342e8e53d7
Umer gull <mohammadmoavia26.com>
2026-05-07 22:06:46 +0500

    finall

--- src/data/companiesData.js
+++ src/data/companiesData.js
@@ -1,11 +1,12 @@
 const companies = [
-    { id: 1, logo: "/assets/company-logo/1.png", name: "Company 1" },
-    { id: 2, logo: "/assets/company-logo/2.png", name: "Company 2" },
-    { id: 3, logo: "/assets/company-logo/3.png", name: "Company 3" },
-    { id: 4, logo: "/assets/company-logo/4.png", name: "Company 4" },
-    { id: 5, logo: "/assets/company-logo/5.png", name: "Company 5" },
-    { id: 6, logo: "/assets/company-logo/6.png", name: "Company 6" },
-    { id: 7, logo: "/assets/company-logo/6.png", name: "Company 6" },
+    { id: 1, logo: "/assets/company-logo/1.png", name: "walls" },
+    { id: 2, logo: "/assets/company-logo/2.png", name: "gsk" },
+    { id: 3, logo: "/assets/company-logo/3.png", name: "deeradari" },
+    { id: 4, logo: "/assets/company-logo/4.png", name: "hilal foods" },
+    { id: 5, logo: "/assets/company-logo/5.png", name: "blue band" },
+    { id: 6, logo: "/assets/company-logo/6.png", name: "jade beauty" },
+    { id: 7, logo: "/assets/company-logo/7.png", name: "vida cosmetic" },
+    { id: 8, logo: "/assets/company-logo/8.png", name: "Loreal groups" },
 ];
 
 export default companies
